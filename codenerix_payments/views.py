e139e6344a671e5b1f21e5ab2805d369ff3d9060
Juanmi Taboada <juanmi@juanmitaboada.com>
2022-05-13 14:12:09 +0200

    Migration to Django 4 ... in progress

--- codenerix_payments/views.py
+++ codenerix_payments/views.py
@@ -25,7 +25,7 @@ from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect
-from django.utils.translation import ugettext_lazy as _, ugettext as __
+from django.utils.translation import gettext_lazy as _, gettext as __
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.shortcuts import render
